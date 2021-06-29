85e921671cc46e7634e2d489021fceb5c4bce9bf
biswapcom <defisysadmin@protonmail.com>
2021-06-29 12:31:44 +0300

    matic factory

--- src/constants.ts
+++ src/constants.ts
@@ -24,12 +24,12 @@ export enum Rounding {
 
 export const Contracts: { [chainId in ChainId]: any} = {
   [ChainId.MATIC]: {
-    factory: '0x858E3312ed3A876947EA49d572A7C42DE08af7EE',
-    init_code: '0xfea293c909d87cd4153593f077b76bb7e94340200f4ee84211ae8e4f9bd7ffdf'
+    factory: '0x2A6332210b992734a1B311Bfe65Ef308225e91Ac',
+    init_code: '0x0f6f23c3e29a036281cb79da4fe87e549e2af18c5d2d130c03b5ab8ecced648c'
   },
   [ChainId.MATIC_TESTNET]: {
-    factory: '0x858E3312ed3A876947EA49d572A7C42DE08af7EE',
-    init_code: '0xfea293c909d87cd4153593f077b76bb7e94340200f4ee84211ae8e4f9bd7ffdf'
+    factory: '0x2A6332210b992734a1B311Bfe65Ef308225e91Ac',
+    init_code: '0x0f6f23c3e29a036281cb79da4fe87e549e2af18c5d2d130c03b5ab8ecced648c'
   },
   [ChainId.BSCTESTNET]: {
     factory: '0x858E3312ed3A876947EA49d572A7C42DE08af7EE',
